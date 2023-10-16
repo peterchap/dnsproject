acde8cf62820669d1a9a7778ee803f734884581d
Peter Chaplin <peterchaplin@btinternet.com>
2023-10-16 21:04:15 +0100

    161023 added s3 parquet write

--- asyncio_dns_ubuntu.py
+++ asyncio_dns_ubuntu.py
@@ -13,6 +13,7 @@ from typing import List
 from loguru import logger as custom_logger
 from datetime import date
 from pyarrow import csv
+from io import BytesIO
 
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0",
@@ -227,12 +228,15 @@ def read_file_from_s3(bucket_name, file_name):
 
 if __name__ == "__main__":
     directory = "/root/dns_project/"
+    # directory = "/home/peter/Downloads/"
     bucket_name = "domain-monitor"
     file_key = "domain_update_daily2023-10-16-05-27-18.zip"
 
     # Path to save the downloaded zip file (this could be /tmp if you're running on AWS Lambda)
     download_path = "/root/dns_project/"
+    # download_path = "/home/peter/Downloads/"
     extract_dir = "/root/dns_project/"
+    # extract_dir = "/home/peter/Downloads/"
     csv_file_name = "domains-detailed-update.csv"
 
     session = boto3.session.Session()
@@ -314,3 +318,38 @@ if __name__ == "__main__":
                 batchcount = batchcount + step
     LOGGER.success(f"Executed Batch in {time.time() - start_time:0.2f} seconds.")
     print("Elapsed time: ", time.time() - start_time)
+
+    # read in arrow file and convert to parquet and export to s3
+
+    def write_dataframe_to_s3_parquet(df, bucket, key, s3_client=None):
+        """Write a dataframe to S3 in Parquet format.
+        :param df: DataFrame to write.
+        :param bucket: S3 bucket name.
+        :param key: S3 key for the file (path + filename).
+        :param s3_client: Pre-configured boto3 S3 client.
+        """
+        if s3_client is None:
+            s3_client = boto3.client("s3")
+
+        # Convert DataFrame to Parquet
+        parquet_buffer = BytesIO()
+        df.to_parquet(parquet_buffer, index=False, engine="pyarrow")
+
+        # Reset buffer cursor
+        parquet_buffer.seek(0)
+
+        # Upload the Parquet file to S3
+        s3_client.put_object(Bucket=bucket, Key=key, Body=parquet_buffer.getvalue())
+        LOGGER.info(f"File uploaded to S3 bucket {bucket} at {key}")
+        print(f"File uploaded to S3 bucket {bucket} at {key}")
+
+    s3_bucket = "domain-monitor-results"
+    s3_key = "domains_added.parquet"
+    with pa.ipc.open_stream(directory + "domains_all.arrow") as reader:
+        print(reader.schema)
+        # for batch in reader:
+
+        df = reader.read_pandas()
+    # df.to_csv(directory + "arrowtest.csv", index=False)  # print(len(batches2))
+    print(df.shape)
+    write_dataframe_to_s3_parquet(df, s3_bucket, s3_key)
