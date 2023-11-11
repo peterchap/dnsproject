4fbc000362afb3549bae86436249fb8b3ee63e50
Peter Chaplin <42451267+peterchap@users.noreply.github.com>
2023-11-11 11:17:48 +0000

    Update asyncio_dns_daily_updates.py
    changed date to datetime object

--- asyncio_dns_daily_updates.py
+++ asyncio_dns_daily_updates.py
@@ -100,14 +100,16 @@ async def execute_fetcher_tasks(urls_select: List[str], create_date, total_count
             "wwwcname",
             "mail",
             "mailptr",
-            "Domain_date",
-            "Create_date",
+            "domain_date",
+            "create_date",
         ]
         for t in tasks:
             data = await t
             res = {keys[y]: data[y] for y in range(12)}
             results.append(res)
         df = pd.DataFrame(results)
+        df['comain_date'] = pd.to_datetime(df['domain_date']
+        df['create_date'] = pd.to_datetime(df['create_date']                                   
         # (print("check ", df.shape))
         # LOGGER.success(
         #  f"Executed Batch in {time.perf_counter() - start_time:0.2f} seconds.")
