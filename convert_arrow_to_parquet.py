6c8118ee35c5e9c93f06f19b3ec91f6cf16df47e
Peter Chaplin <42451267+peterchap@users.noreply.github.com>
2023-11-11 20:47:40 +0000

    Update convert_arrow_to_parquet.py
    fixed arrow read

--- convert_arrow_to_parquet.py
+++ convert_arrow_to_parquet.py
@@ -5,7 +5,8 @@ directory = '/root/dnsproject/'
 file = 'domains_all.arrow'
 output = 'domains_all.parquet'
 # Read arrow file from disk
-table = pa.Table.from_batches(pa.ipc.open_file(directory + file).read_all())
+with pa.ipc.open_stream(directory+file) as f:
+    table = f.read_all()
 
 # Write arrow table to parquet file
 pq.write_table(table, directory + output)
