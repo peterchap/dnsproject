6057f19224108a8e29e2a0a26129c449a6e55140
Peter Chaplin <peterchaplin@btinternet.com>
2023-11-11 18:13:10 +0000

    new arrow to parquet script

---
+++ convert_ arrow_to_parquet.py
@@ -0,0 +1,12 @@
+
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+directory = '/root/dnsproject/'
+file = 'domains_all.arrow'
+output = 'domains_all.parquet'
+# Read arrow file from disk
+table = pa.Table.from_batches(pa.ipc.open_file(directory + file).read_all())
+
+# Write arrow table to parquet file
+pq.write_table(table, directory + output)
