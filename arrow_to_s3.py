e8e846123060efbaf4317c394cb8b603a71f7c7b
Peter Chaplin <peterchaplin@btinternet.com>
2023-11-12 17:11:47 +0000

    dn_db process startpoint

---
+++ arrow_to_s3.py
@@ -0,0 +1,32 @@
+import boto3
+import os
+import time
+import pyarrow as pa
+import pyarrow.parquet as pq
+import s3fs
+
+# Function to convert Arrow file to Parquet and upload to S3
+def arrow_to_s3_parquet(arrow_file_path, s3_bucket_name, s3_parquet_key):
+    # Read the Arrow file from disk
+    with pa.ipc.open_stream(arrow_file_path) as f:
+        table = f.read_all()
+
+    fs =s3fs.S3FileSystem(anon=False)
+    s3_path = f"s3://{s3_bucket_name}/{s3_parquet_key}"
+    with fs.open(s3_path, "wb") as s3_file:
+        pq.write_table(table, s3_file)
+
+directory = "/root/dnsproject/"
+file = "domains_all.arrow"
+
+s3_bucket = "domain-monitor-results"
+host = os.uname()[1]
+hostname = host.split(".")[0]
+timestamp = time.time()
+s3_filename = f"dns_result_{hostname}_{timestamp:0.0f}.parquet"
+s3_key = s3_filename
+
+# Execute the function
+arrow_to_s3_parquet(directory + file, s3_bucket, s3_key)
+
+print(f"File uploaded to S3 bucket {s3_bucket} at {s3_key}")
